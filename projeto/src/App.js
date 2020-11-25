6bbbb09a6feb51976d6e4083a138a6166a529b30
superleor <superleor30@hotmail.com>
2020-11-25 18:22:08 -0300

    Rearranjo de arquivos

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import MeusPedidos from './MeusPedidos'
+import { Route, Switch } from "react-router-dom"
+import Drawer from "./Drawer"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles({
+  container: {
+    display: "flex"
+  }
+});
 
 function App() {
+  const classes = useStyles()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+    <Drawer />
+    <Switch>
+      <Route exact from="/" render={props => <MeusPedidos {...props} />} />
+    </Switch>
     </div>
   );
 }
---
+++ projeto/src/DadosPedidos.js
@@ -0,0 +1,27 @@
+import { makeStyles } from '@material-ui/core/'
+
+
+const useStyles = makeStyles(theme =>({
+
+}))
+
+function DadosPedidos(){
+
+    const classes = useStyles()
+
+
+    return(
+
+        <div>
+            <h4>Numero de Quarto</h4>
+            
+            <h5>Nome</h5>
+
+        </div>
+
+    )
+
+
+}
+
+export default DadosPedidos
