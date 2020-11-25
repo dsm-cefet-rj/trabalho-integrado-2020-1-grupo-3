988ab6378694107bd9e88d062fe644db1c03b3b9
superleor <superleor30@hotmail.com>
2020-11-25 18:39:27 -0300

    rearranjo 2

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import MeusPedidos from './MeusPedidos'
-import { Route, Switch } from "react-router-dom"
-import Drawer from "./Drawer"
-import { makeStyles } from "@material-ui/core/styles"
-
-const useStyles = makeStyles({
-  container: {
-    display: "flex"
-  }
-});
 
 function App() {
-  const classes = useStyles()
   return (
-    <div className={classes.container}>
-    <Drawer />
-    <Switch>
-      <Route exact from="/" render={props => <MeusPedidos {...props} />} />
-    </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
