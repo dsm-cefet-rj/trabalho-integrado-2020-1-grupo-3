f85efdefdabb5e88ccb8ebd8c77504d386c6f87c
superleor <superleor30@hotmail.com>
2020-11-26 14:55:51 -0300

    calendário de eventos

--- projeto/src/Componentes/EventoContainer.js
+++ projeto/src/Componentes/EventoContainer.js
@@ -1,20 +1,41 @@
 import eventos from './EventosEx';
 import EventoComponent from './EventoComponent'
 import React, {Component} from 'react'
+import Calendar from 'react-calendar';
 
 class EventoContainer extends Component{
-  
     constructor(){
       super()
       this.state={
-        eventos: eventos
+        eventos: eventos,
+        date: new Date()
       }
     }
+
+    onChange = date => this.setState({ date })
   
     render(){
-        const todosEventos = this.state.eventos.map(a => <EventoComponent chave={a.id} evento={a}/>)
+         const diaSemana = this.state.date.getUTCDay()
+         const todosEventos = this.state.eventos.map(a =>{
+          if (diaSemana === 1 && a.id === "1"){
+            return null
+          }else if (diaSemana === 2 && a.id === "2"){
+            return null
+          } else if (diaSemana === 3 && a.id === "3"){
+            return null
+          } else {
+            return <EventoComponent chave={a.id} evento={a}/>
+          }
+        } )
+       
       return (
-          <div>{todosEventos}</div>
+          <div>
+            <Calendar
+              onChange={this.onChange}
+              value={this.state.date}
+            />
+            {todosEventos}
+          </div>
       )
     }
   }
